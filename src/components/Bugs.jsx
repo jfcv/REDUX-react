ce9283f73de76fe54e1d5510b73585141d096319
Juan Cuadros <juan.cuadros@pm.me>
2022-04-05 22:29:26 -0500

    Implement the React - Redux application set up using [react-redux@7.2]. [Provider, connect()] are used to set up the store and getting the data as properties at the children React Components.

--- src/components/Bugs.jsx
+++ src/components/Bugs.jsx
@@ -1,24 +1,15 @@
 import React, { Component, useContext, useEffect, useState } from "react";
+import { connect } from "react-redux";
 import StoreContext from "../contexts/storeContext";
 import { loadBugs } from "../store/bugs";
 
-const Bugs = () => {
-  let bugsInStore;
-  const store = useContext(StoreContext);
-  const [bugs, setBugs] = useState([]);
-
+/**
+ * Presentation Component : just presents data !
+ */
+const Bugs = ({ bugs, loadBugs }) => {
   useEffect(() => {
-    const unsubscribe = store.subscribe(() => {
-      bugsInStore = store.getState().entities.bugs.list;
-      if (bugs !== bugsInStore) setBugs([...bugsInStore]);
-    });
-
-    store.dispatch(loadBugs());
-
-    return () => {
-      unsubscribe();
-    };
-  }, [bugsInStore]);
+    loadBugs();
+  }, []);
 
   return (
     <div>
@@ -32,4 +23,28 @@ const Bugs = () => {
   );
 };
 
-export default Bugs;
+/**
+ * takes the state of the store
+ * returns the part of the store that we're
+ * interested in
+ * properties of this object are gonna end up
+ * as props of our component
+ */
+const mapStateToProps = (state) => ({
+  bugs: state.entities.bugs.list,
+});
+
+/**
+ * takes the dispatch fn of the store and
+ * map it to the props of our component
+ * properties of this object are gonna end up
+ * as props of our component
+ */
+const mapDispatchToProps = (dispatch) => ({
+  loadBugs: () => dispatch(loadBugs()),
+});
+
+/**
+ * Container Component : wraps the Presentation Component (Bugs )
+ */
+export default connect(mapStateToProps, mapDispatchToProps)(Bugs);
